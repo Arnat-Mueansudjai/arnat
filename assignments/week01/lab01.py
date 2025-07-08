04d9a7d2f5fffcfd1064f1db2ab406d14b9ecb42
Arnat-Mueansudjai <arnat.m@ku.th>
2025-07-08 12:13:55 +0700

    homework01

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,13 @@
+print("=== Personal Profile Creator ===")
+name = input("What is your name?:")
+age = input("How old are you?:")
+mail = input("What is your email address?:")
+num = input("What is your phone number?:")
+hobby = input("What is rrrryour favorite hobby?:")
+
+print("===Your profile===")
+print("Name: ",name)
+print("Age: ",age)
+print("Email: ",mail)
+print("Phone number: ",num)
+print("Favorite Hobby: ",hobby)
