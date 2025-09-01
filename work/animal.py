b323d05f1de5937136daefaba1e88e04bb8d8c77
Arnat-Mueansudjai <arnat.m@ku.th>
2025-09-01 11:28:56 +0700

    a

--- work/animal.py
+++ work/animal.py
@@ -12,13 +12,3 @@ class Mammal(Animal):
 
     def __init__(self, name):
         super().__init__(name)
-
-class Bear(Mammal):
-
-    def __init__(self, name):
-        super().__init__(name)
-
-class Gorilla(Mammal):
-
-    def __init__(self, name):
-        super().__init__(name)
