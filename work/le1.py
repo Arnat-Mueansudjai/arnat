8d851b2a1b9eaadd144e65cdeaec6d6f26b45520
Arnat-Mueansudjai <arnat.m@ku.th>
2025-09-08 11:40:41 +0700

    1

---
+++ work/le1.py
@@ -0,0 +1,15 @@
+class A:
+def m1(self,a):
+print(a)
+
+def m1(self,a,b):
+print(a + b)
+
+def m1(self, name , age , a):
+
+print('Name is :' , name)
+print('Age is :' , age)
+print(a)
+
+obj = A()
+obj.m1('Avinesh',30,50)
